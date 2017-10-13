0071718b19dc0eab23aefd2b64dca5c2cd360025
Jasper <choujs@uci.edu>
2017-10-12 18:01:53 -0700

    Update deploy-ambari-centos-6.8.sh

--- deploy-ambari-centos-6.8.sh
+++ deploy-ambari-centos-6.8.sh
@@ -309,6 +309,14 @@ do
 
         # Update Hostname in /etc/sysconfig/network on the DataNode.
         ssh -o 'StrictHostKeyChecking no' root@$HOST sed -i -e "s/HOSTNAME=$HOST/HOSTNAME=$HOST.$DOMAIN_NAME/" /etc/sysconfig/network
+        
+        ssh -o 'StrictHostKeyChecking no' root@$HOST << EOF
+		        echo -e '\nn\np\n1\n\n\nw' | fdisk /dev/sdc
+        		mkfs -t ext4 /dev/sdc1 
+		        mkdir -p /hadoop/hdfs/data
+	        	mount /dev/sdc1 /hadoop/hdfs/data
+	        	df
+      	EOF
 
         # Restarting the network service on the DataNode.
         ssh -o 'StrictHostKeyChecking no' root@$HOST /etc/init.d/network restart
