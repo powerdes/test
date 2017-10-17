5c9ef66b9ed79473b7681d46f1e45b84680d34a6
sjshin1 <sjshin1@users.noreply.github.com>
2017-10-17 09:44:04 -0700

    Updated ambari repo to 2.5.2.0

--- changed2.sh
+++ changed2.sh
@@ -130,7 +130,7 @@ yum install openssh-server -y
 yum install expect -y
 
 # Downloading the Ambari repository file.
-wget -nv http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.4.2.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+wget -nv http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.5.2.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
 
 # Installing Ambari Server.
 yum install ambari-server -y
